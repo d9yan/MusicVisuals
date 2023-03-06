bfe3db2e4cee3a157ce8cd7120ea4b9a557d736f
elicookies <elikoleva32@gmail.com>
2023-03-06 17:04:38 +0000

    hello

--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -1,21 +1,19 @@
 package ie.tudublin;
-
-import example.CubeVisual;
-import example.MyVisual;
-import example.RotatingAudioBands;
+import c21320836.EliVisual;
 
 public class Main
-{	
+{
 
-	public void startUI()
+	public static void startUI()
 	{
 		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new MyVisual());		
-	}
+        processing.core.PApplet.runSketch( a, new EliVisual());
+    }
 
 	public static void main(String[] args)
 	{
 		Main main = new Main();
-		main.startUI();			
+		main.startUI();	
 	}
+	
 }
