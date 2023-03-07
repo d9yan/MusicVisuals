cb523f68eaf9a8adcd0c9cc317295dca4f8f8318
Raiyan Mohd Farid <raiyanmfarid@gmail.com>
2023-03-07 01:47:32 +0000

    Goofy

--- java/src/C21385263/visual.java
+++ java/src/C21385263/visual.java
@@ -2,6 +2,6 @@ package c21385263;
 
 public class visual {
     public static void main(String[] args) {
-        System.out.println("Stinky fart");
+        System.out.println("Stinky fart XP");
     }
 }
